85bb29298bbfdc1b89568c475876bcffce31dcaa
simon <simonjhunter@outlook.com>
2018-10-23 16:10:50 +0100

    Added basic routing and guards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,36 @@ import {FormsModule} from '@angular/forms';
 import {DisplayPricePipe} from './shared/display-price.pipe';
 import {StarComponent} from './shared/star.component';
 import {HttpClientModule} from '@angular/common/http';
+import { SiteDetailComponent } from './sites/site-detail.component';
+import {WelcomeComponent} from './home/welcome.component';
+import {RouterModule} from '@angular/router';
+import {SiteDetailGuard} from './sites/site-detail.guard';
+import { SiteNosuchsiteComponent } from './sites/site-nosuchsite.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SiteListComponent,
     DisplayPricePipe,
-    StarComponent
+    StarComponent,
+    SiteDetailComponent,
+    WelcomeComponent,
+    SiteNosuchsiteComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'sites', component: SiteListComponent},
+      {path: 'sites/:id',
+        canActivate: [SiteDetailGuard],
+        component: SiteDetailComponent},
+      {path: 'welcome', component: WelcomeComponent},
+      {path: 'nosite', component: SiteNosuchsiteComponent},
+      {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+      {path: '**', redirectTo: 'welcome', pathMatch: 'full'},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
