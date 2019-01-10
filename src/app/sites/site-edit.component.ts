373a79e256a25aa319e5892b3d8f93669a067298
SimonJHunter <simonjhunter@outlook.com>
2019-01-10 14:58:33 +0000

    Returning data from in memory DB and fixed Add Site issue.

---
+++ src/app/sites/site-edit.component.ts
@@ -0,0 +1,211 @@
+import { Component, OnInit, AfterViewInit, OnDestroy, ViewChildren, ElementRef } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, FormArray, Validators, FormControlName } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Observable, Subscription, fromEvent, merge, of } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+
+import { Site } from './site';
+import { SiteService } from './site.service';
+
+
+import { NumberValidators } from '../shared/number-validator';
+import { GenericValidator } from '../shared/generic-validator';
+
+@Component({
+   templateUrl: './site-edit.component.html',
+   styleUrls: ['./site-edit.component.css']
+ })
+
+export class SiteEditComponent implements OnInit, AfterViewInit, OnDestroy {
+  @ViewChildren(FormControlName, { read: ElementRef }) formInputElements: ElementRef[];
+
+  pageTitle = 'Site Edit';
+  errorMessage: string;
+  siteForm: FormGroup;
+
+  categoryOptions = ['Wild Camping', 'Designated Camping'];
+
+  site: Site;
+  private sub: Subscription;
+
+  // Use with the generic validation message class
+  displayMessage: { [key: string]: string } = {};
+  private validationMessages: { [key: string]: { [key: string]: string } };
+  private genericValidator: GenericValidator;
+
+  get facilities(): FormArray {
+    return <FormArray>this.siteForm.get('facilities');
+  }
+
+  constructor(private fb: FormBuilder,
+               private route: ActivatedRoute,
+               private router: Router,
+               private siteService: SiteService
+  ) {
+//
+//     // Defines all of the validation messages for the form.
+//     // These could instead be retrieved from a file or database.
+    this.validationMessages = {
+      siteName: {
+        required: 'Site name is required.',
+        minlength: 'Site name must be at least three characters.',
+        maxlength: 'Site name cannot exceed 50 characters.'
+      },
+      category: {
+        required: 'A Category is required.'
+      },
+      location: {
+        required: 'A Location is required.'
+      },
+      shortDescription: {
+        required: 'A short description is required.'
+      },
+      starRating: {
+        range: 'Rate the site between 1 (lowest) and 5 (highest).'
+      }
+    };
+//     // Define an instance of the validator for use with this form,
+//     // passing in this form's set of validation messages.
+     this.genericValidator = new GenericValidator(this.validationMessages);
+   }
+
+  ngOnInit(): void {
+    this.siteForm = this.fb.group({
+      siteName: ['', [Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(50)]],
+      category: ['', Validators.required],
+      location: ['', Validators.required],
+      shortDescription: ['', Validators.required],
+      description: '',
+      price: [''],
+      starRating: ['', NumberValidators.range(1, 5)],
+      facilities: this.fb.array([])
+    });
+
+    // Read the product Id from the route parameter
+     this.sub = this.route.params.subscribe(
+       params => {
+         const id = +params['id'];
+         this.getSite(id);
+       }
+     );
+  }
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
+  ngAfterViewInit(): void {
+    // Watch for the blur event from any input element on the form.
+    const controlBlurs: Observable<any>[] = this.formInputElements
+      .map((formControl: ElementRef) => fromEvent(formControl.nativeElement, 'blur'));
+
+    // Merge the blur event observable with the valueChanges observable
+    merge(this.siteForm.valueChanges, ...controlBlurs).pipe(
+      debounceTime(800)
+    ).subscribe(value => {
+      this.displayMessage = this.genericValidator.processMessages(this.siteForm);
+    });
+  }
+
+  // Add and Delete facility rows.
+  addFacility(): void {
+    this.facilities.push(new FormControl());
+  }
+
+  deleteFacility(index: number): void {
+    this.facilities.removeAt(index);
+    this.facilities.markAsDirty();
+  }
+
+  getSite(id: number): void {
+    this.siteService.getSite(id)
+      .subscribe(
+        (site: Site) => this.displaySite(site),
+        (error: any) => this.errorMessage = <any>error
+      );
+  }
+
+  displaySite(site: Site): void {
+    if (this.siteForm) {
+      this.siteForm.reset();
+    }
+    this.site = site;
+
+    if (this.site == null) {
+      this.pageTitle = 'Add Site';
+    } else {
+      this.pageTitle = `Edit Site: ${this.site.name}`;
+
+
+      // Update the data on the form
+      this.siteForm.patchValue({
+        siteName: this.site.name,
+        category: this.site.category,
+        location: this.site.location,
+        shortDescription: this.site.shortDescription,
+        description: this.site.description,
+        price: this.site.price,
+        starRating: this.site.starRating
+        // facilities: string;
+      });
+
+      this.siteForm.setControl('facilities', this.fb.array(this.site.facilities || []));
+    }
+}
+
+  deleteSite(): void {
+    if (this.site.siteId === 0) {
+      // Don't delete, it was never saved.
+      this.onSaveComplete();
+    } else {
+      if (confirm(`Really delete the site: ${this.site.name}?`)) {
+        this.siteService.deleteSite(this.site.siteId)
+          .subscribe(
+            () => this.onSaveComplete(),
+            (error: any) => this.errorMessage = <any>error
+          );
+      }
+    }
+  }
+
+  saveSite(): void {
+    if (this.siteForm.valid) {
+      if (this.siteForm.dirty) {
+        // Copy over all of the original product properties
+        // Then copy over the values from the form
+        // This ensures values not on the form, such as the Id, are retained
+        const p = { ...this.site, ...this.siteForm.value };
+
+        if (p.siteId === 0) {
+          this.siteService.createSite(p)
+            .subscribe(
+              () => this.onSaveComplete(),
+              (error: any) => this.errorMessage = <any>error
+            );
+        } else {
+          this.siteService.updateSite(p)
+            .subscribe(
+              () => this.onSaveComplete(),
+              (error: any) => this.errorMessage = <any>error
+            );
+        }
+      } else {
+        this.onSaveComplete();
+      }
+    } else {
+      this.errorMessage = 'Please correct the validation errors.';
+    }
+   }
+
+  onSaveComplete(): void {
+     // Reset the form to clear the flags
+    this.siteForm.reset();
+    this.router.navigate(['/sites']);
+   }
+
+
+}
+
+
